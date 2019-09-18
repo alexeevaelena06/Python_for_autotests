c6fa15067f5f0981bc3ed06cd902a42fb06c5094
alexeevaelena06 <alexeevaelena06@gmail.com>
2019-09-18 23:38:24 +0300

    add test_delete_first_group

--- Fixture/group.py
+++ Fixture/group.py
@@ -5,19 +5,32 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
-    def check_record_of_group(self):
+    def return_to_group_page(self):
         wd = self.app.wd
         wd.find_element(By.LINK_TEXT, "group page").click()
-        wd.find_element(By.CSS_SELECTOR, ".group").click()
 
     def create(self, group):
         wd = self.app.wd
         wd.find_element(By.LINK_TEXT, "groups").click()
+        # init group creation
         wd.find_element(By.NAME, "new").click()
+        # fill group form
         wd.find_element(By.NAME, "group_name").click()
         wd.find_element(By.NAME, "group_name").send_keys(group.groupname)
         wd.find_element(By.NAME, "group_header").click()
         wd.find_element(By.NAME, "group_header").send_keys(group.header)
         wd.find_element(By.NAME, "group_footer").click()
         wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        # submit group
         wd.find_element(By.NAME, "submit").click()
+        # return to group page
+        self.return_to_group_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        wd.find_element(By.LINK_TEXT, "groups").click()
+        # choose group
+        wd.find_element(By.NAME, "selected[]").click()
+        # delete group
+        wd.find_element(By.NAME, "delete").click()
+        self.return_to_group_page()
--- Tests/test_add_new_group.py
+++ Tests/test_add_new_group.py
@@ -1,24 +1,13 @@
-import pytest
-from Fixture.application import Application
 from Model.group import Group
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_1_add_new_group(app):
     app.session.login(login="admin", password="secret")
     app.group.create(Group("group_2", "ghghgh", "ghhghghgh"))
-    app.group.check_record_of_group()
     app.session.logout()
 
 
 def test_2_add_new_group(app):
     app.session.login("admin", "secret")
     app.group.create(Group("", "", ""))
-    app.group.check_record_of_group()
     app.session.logout()
