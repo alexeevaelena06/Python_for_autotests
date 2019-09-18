6f497d7124a51cdf49b7586f5eded51b5ec8b8da
alexeevaelena06 <alexeevaelena06@gmail.com>
2019-09-18 23:43:07 +0300

    add scope="session" in conftest.py

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from Fixture.application import Application
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
