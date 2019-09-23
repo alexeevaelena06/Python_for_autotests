5dc0ec1303968570b81a9ce4a28e5ed968d16f69
alexeevaelena06 <alexeevaelena06@gmail.com>
2019-09-23 22:10:48 +0300

    modify test_modify_group.py

--- Fixture/group.py
+++ Fixture/group.py
@@ -11,7 +11,7 @@ class GroupHelper:
 
     def open_group_page(self):
         wd = self.app.wd
-        if not wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0:
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
             wd.find_element(By.LINK_TEXT, "groups").click()
 
     def create(self, group):
--- Tests/test_modify_group.py
+++ Tests/test_modify_group.py
@@ -2,4 +2,6 @@ from Model.group import Group
 
 
 def test_modify_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(group_name="old_group", group_header="old_header", group_footer="old_footer"))
     app.group.modify_first_group(Group(group_name="New_name_group"))
