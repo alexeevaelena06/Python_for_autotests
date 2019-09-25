fe55f87bb310d7f5afde43db7a275a5c3786f40e
alexeevaelena06 <alexeevaelena06@gmail.com>
2019-09-25 21:49:48 +0300

    add sorted lists

--- Tests/test_add_new_group.py
+++ Tests/test_add_new_group.py
@@ -3,13 +3,19 @@ from Model.group import Group
 
 def test_1_add_new_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(group_name="group_2", group_header="ghghgh", group_footer="ghhghghgh"))
+    group = Group(group_name="group_2", group_header="ghghgh", group_footer="ghhghghgh")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_2_add_new_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group("", "", ""))
+    group = Group()
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
--- Tests/test_modify_group.py
+++ Tests/test_modify_group.py
@@ -5,6 +5,10 @@ def test_modify_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(group_name="old_group", group_header="old_header", group_footer="old_footer"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(group_name="New_name_group"))
+    group = Group(group_name="New_name_group")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
