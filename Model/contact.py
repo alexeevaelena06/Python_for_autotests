4a3abbf273e2f1337651482b8fe9ed2ea81cdc7a
alexeevaelena06 <alexeevaelena06@gmail.com>
2019-10-02 21:33:12 +0300

    modify Model/contact.py

--- Model/contact.py
+++ Model/contact.py
@@ -1,25 +1,27 @@
+from sys import maxsize
+
+
 class Contact:
-    def __init__(self, name, middle_name, last_name, nick_name, title, my_company, address, home_phone,
-                 mobile_phone, work_phone, fax, email_1, email_2, email_3, bth_day, bth_month, bthday_year, a_year,
-                 second_address, home_phone2, notes):
-        self.name = name
-        self.middle_name = middle_name
-        self.last_name = last_name
-        self.nick_name = nick_name
-        self.title = title
-        self.my_company = my_company
-        self.address = address
-        self.home_phone = home_phone
-        self.mobile_phone = mobile_phone
-        self.work_phone = work_phone
-        self.fax = fax
-        self.email_1 = email_1
-        self.email_2 = email_2
-        self.email_3 = email_3
-        self.bth_day = bth_day
-        self.bth_month = bth_month
-        self.bthday_year = bthday_year
-        self.a_year = a_year
-        self.second_address = second_address
-        self.home_phone2 = home_phone2
-        self.notes = notes
+    def __init__(self, firstname=None, lastname=None, homephone=None, mobilephone=None, workphone=None,
+                 secondryphone=None, id=None):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.workphone = workphone
+        self.secondryphone = secondryphone
+        self.id = id
+
+    def __repr__(self):
+        return f"{self.id}:{self.firstname} {self.lastname}"
+
+    def __eq__(self, other):
+        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname \
+               and self.lastname == other.lastname
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
+
