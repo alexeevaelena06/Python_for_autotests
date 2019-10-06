f851bab8f2d7849e76a9c6ea418ec7cb10f69abf
alexeevaelena06 <alexeevaelena06@gmail.com>
2019-10-06 22:46:59 +0300

    change conftest.py, application.py

--- Fixture/application.py
+++ Fixture/application.py
@@ -1,16 +1,23 @@
 from selenium import webdriver
 from Fixture.contact import ContactHelper
 from Fixture.group import GroupHelper
-from Fixture.locators import admin_page
 from Fixture.session import SessionHelper
 
 
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Chrome()
+    def __init__(self, browser, base_url):
+        if browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def destroy(self):
         self.wd.quit()
@@ -21,7 +28,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get(admin_page)
+        wd.get(self.base_url)
 
     def is_valid(self):
         try:
--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from Fixture.application import Application
+from Fixture.locators import admin_page
 
 fixture = None
 
@@ -7,11 +8,13 @@ fixture = None
 @pytest.fixture(scope="session")
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -22,3 +25,8 @@ def stop(request):
         fixture.session.ensure_logout()
         fixture.destroy()
     request.addfinalizer(fin)
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--baseUrl", action="store", default=admin_page)
