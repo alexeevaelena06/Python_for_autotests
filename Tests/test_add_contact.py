f3e12bdd4e08bd4f5892748813a77f5af1e5d9fe
alexeevaelena06 <alexeevaelena06@gmail.com>
2019-10-06 23:10:23 +0300

    modify test_add_contact.py

--- Tests/test_add_contact.py
+++ Tests/test_add_contact.py
@@ -1,6 +1,24 @@
 from Model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_test_add_contact(app):
-    app.contact.create(Contact("Name_1", "LastName_1", "NickName_1", "555555", "9261664735", "4956753452",
-                               "8888888888"))
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters+string.digits+string.punctuation+" "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_digit(maxlen):
+    symbols = string.digits*11
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 10),
+                    homephone=random_digit(11), workphone=random_digit(11),
+                    mobilephone=random_digit(11), secondryphone=random_digit(11))]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_test_add_contact(app, contact):
+    app.contact.create(contact)
