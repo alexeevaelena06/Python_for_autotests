f1d0096865fe0994424dfd302a8d4b88a5a6c5fe
alexeevaelena06 <alexeevaelena06@gmail.com>
2019-10-10 09:42:44 +0300

    add contacts generator

---
+++ Data/contacts.py
@@ -0,0 +1,7 @@
+from Model.contact import Contact
+
+
+testdata = [Contact(firstname="Name1", lastname="LastName1", homephone="111", workphone="1111", mobilephone="11111",
+                    secondryphone="111111"),
+            Contact(firstname="Name2", lastname="LastName2", homephone="222", workphone="2222", mobilephone="22222",
+                    secondryphone="222222")]
--- Tests/test_add_contact.py
+++ Tests/test_add_contact.py
@@ -1,24 +1,10 @@
-from Model.contact import Contact
-import pytest
-import random
-import string
+# from Model.contact import Contact
+# import pytest
+# import random
+# import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters+string.digits+string.punctuation+" "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-def random_digit(maxlen):
-    symbols = string.digits*11
-    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 10),
-                    homephone=random_digit(11), workphone=random_digit(11),
-                    mobilephone=random_digit(11), secondryphone=random_digit(11))]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_test_add_contact(app, contact):
+# @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_test_add_contact(app, json_contacts):
+    contact = json_contacts
     app.contact.create(contact)
