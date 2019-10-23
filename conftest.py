3f56006c39b837d1845d6cd564d57e49e983df74
alexeevaelena06 <alexeevaelena06@gmail.com>
2019-10-23 20:59:04 +0300

    add test_db_matches_ui (loading object from db)

--- conftest.py
+++ conftest.py
@@ -14,15 +14,16 @@ def load_config(file):
     global target
     if target is None:
         config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
-        with open(config_file) as f:
+        with open(config_file, encoding="UTF-8") as f:
             target = json.load(f)
     return target
+
 
 @pytest.fixture
 def app(request):
     global fixture
     browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))['web']
+    web_config = load_config(request.config.getoption("--target"))["web"]
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=web_config["baseUrl"])
         fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
@@ -31,8 +32,8 @@ def app(request):
 
 @pytest.fixture(scope="session")
 def db(request):
-    db_config = load_config(request.config.getoption("--target"))['db']
-    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
+    db_config = load_config(request.config.getoption("--target"))["db"]
+    dbfixture = DbFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config["password"])
     def fin():
         dbfixture.destroy()
     request.addfinalizer(fin)
