1236a252ca2587e4090aa3d3baac66083051c570
alexeevaelena06 <alexeevaelena06@gmail.com>
2019-10-31 09:14:39 +0300

    modify test_add_new_group.py

--- Tests/test_add_new_group.py
+++ Tests/test_add_new_group.py
@@ -6,9 +6,10 @@ from Model.group import Group
 # @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_new_group(app, db, json_groups):
     group = json_groups
-    old_groups = db.get_group_list()
+    old_groups = app.group.get_group_list()
     app.group.create(group)
     # assert len(old_groups) + 1 == app.group.count()
     new_groups = db.get_group_list()
     old_groups.append(group)
+    # TODO подправить проверку
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
